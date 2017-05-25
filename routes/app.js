509926d751be65fcb4fb5126af78946a13f7b328
happyahluwalia <happyahluwalia@gmail.com>
2017-05-25 17:08:12 -0400

    Cleanup of test mongo db code

--- routes/app.js
+++ routes/app.js
@@ -1,32 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-var User = require('../model/user');
-
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  User.findOne({}, function(err, result) {
-     if(err) {
-       res.send('Error Bhai');
-     }
-     res.render('message', {email: result.email});
-  })
-  
+     res.render('message');
 });
-
-
-router.post('/', function(req, res, next){
-  var email = req.body.email;
-  var user = new User({
-    firstName: 'Happy',
-    lastName: 'Singh',
-    password: 'shh... its a secret',
-    email: email
-  });
-
-  user.save();
-
-  res.redirect('/');
-})
 
 module.exports = router;
